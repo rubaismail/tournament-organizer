278fc6f62d530a826b5b2ba1e2a58cc62724a0e1
Ruba <134463376+rubaismail@users.noreply.github.com>
2025-04-09 00:22:24 -0400

    Update and rename main.c to P2.c

--- P2.c
+++ P2.c
@@ -1,8 +1,6 @@
-/* COP 3502C Assignment 2
-* This program is written by: Ruba Ismail
-* UCF ID: 5186181
+/* 
+* This program is written by Ruba Ismail
 * Date: 2/5/2025
-* Class: COP 3205C, Professor Tanvir
 * Purpose: process ticket lines as queues
 */
 
